1dd6768f286ef1984808f1574cefeba7bc17cdd1
Semjo <semjomagene@gmail.com>
2025-02-19 18:30:55 +0200

    feat(layout): implement header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,4 +1,21 @@
+import Image from 'next/image';
+
 const Header = () => {
-  return <header className="h-f-container">Header</header>;
+  return (
+    <header className="h-f-container flex justify-between items-center">
+      <div className="flex gap-2 p-2 pr-5 cursor-pointer rounded-full items-center header-logo">
+        <Image src={'/icons/gtAI.svg'} alt="GtAI logo" height={50} width={50} />
+        <p className="text-3xl font-bold text-white logo-name">GText AI</p>
+      </div>
+      <picture>
+        <Image
+          src={'/icons/clear.png'}
+          alt="clear chat"
+          height={40}
+          width={40}
+        />
+      </picture>
+    </header>
+  );
 };
 export default Header;

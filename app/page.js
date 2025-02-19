aa2cfaa150fab180927d4ec24daadb6e2fcdda1f
Semjo <semjomagene@gmail.com>
2025-02-19 15:58:58 +0200

    chore: initialize the project folder structure

--- app/page.js
+++ app/page.js
@@ -1,3 +1,13 @@
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import Main from '@/components/Main';
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="flex justify-center p-2">
+      <p className="max-w-[45rem] text-center">
+        GText AI may contain mistakes, we recommand checking important
+        informations
+      </p>
+    </footer>
+  );
+};
+export default Footer;

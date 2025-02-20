0767283415bf6efcde659d8c454f8c1f9bd8602a
Semjo <semjomagene@gmail.com>
2025-02-20 10:15:22 +0200

    feat(accessibility): Implement hover and active effects

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,16 +3,22 @@ import Image from 'next/image';
 const Header = () => {
   return (
     <header className="h-f-container flex justify-between items-center absolute top-0">
-      <div className="flex gap-2 p-2 pr-5 cursor-pointer rounded-full items-center header-logo">
-        <Image src={'/icons/gtAI.svg'} alt="GtAI logo" height={50} width={50} />
-        <p className="text-3xl font-bold text-white logo-name">GText AI</p>
+      <div
+        className="flex gap-2 p-2 pr-5 cursor-pointer rounded-full items-center header-logo hover:shadow-btnShad"
+        tabIndex={1}>
+        <Image src={'/icons/gtAI.svg'} alt="GtAI logo" height={40} width={40} />
+        <p className="text-2xl sm:text-3xl font-bold text-white logo-name">
+          GText AI
+        </p>
       </div>
-      <picture className="p-2">
+      <picture
+        className="p-[10px] rounded-full cursor-pointer clear-logo hover:scale-105"
+        tabIndex={1}>
         <Image
           src={'/icons/clear.png'}
           alt="clear chat"
-          height={40}
-          width={40}
+          height={36}
+          width={36}
         />
       </picture>
     </header>
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,61 +1,88 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ["class"],
-    content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-  	extend: {
-  		colors: {
-  			background: 'hsl(var(--background))',
-  			foreground: 'hsl(var(--foreground))',
-  			card: {
-  				DEFAULT: 'hsl(var(--card))',
-  				foreground: 'hsl(var(--card-foreground))'
-  			},
-  			popover: {
-  				DEFAULT: 'hsl(var(--popover))',
-  				foreground: 'hsl(var(--popover-foreground))'
-  			},
-  			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
-  			},
-  			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
-  			},
-  			muted: {
-  				DEFAULT: 'hsl(var(--muted))',
-  				foreground: 'hsl(var(--muted-foreground))'
-  			},
-  			accent: {
-  				DEFAULT: 'hsl(var(--accent))',
-  				foreground: 'hsl(var(--accent-foreground))'
-  			},
-  			destructive: {
-  				DEFAULT: 'hsl(var(--destructive))',
-  				foreground: 'hsl(var(--destructive-foreground))'
-  			},
-  			border: 'hsl(var(--border))',
-  			input: 'hsl(var(--input))',
-  			ring: 'hsl(var(--ring))',
-  			chart: {
-  				'1': 'hsl(var(--chart-1))',
-  				'2': 'hsl(var(--chart-2))',
-  				'3': 'hsl(var(--chart-3))',
-  				'4': 'hsl(var(--chart-4))',
-  				'5': 'hsl(var(--chart-5))'
-  			}
-  		},
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		}
-  	}
+    extend: {
+      boxShadow: {
+        btnShad:
+          'rgba(132, 132, 132, 0.12) 0px 2px 4px 0px, rgba(132, 132, 132, 0.32) 0px 2px 16px 0px',
+      },
+      keyframes: {
+        sweep: {
+          '0%': {
+            transform: 'rotateZ(0deg) translateX(0)',
+          },
+          '25%': {
+            transform: 'rotateZ(-20deg) translateX(-15px)',
+          },
+          '50%': {
+            transform: 'rotateZ(10deg) translateX(10px)',
+          },
+          '75%': {
+            transform: 'rotateZ(-20deg) translateX(-15px)',
+          },
+          '100%': {
+            transform: 'rotateZ(0deg) translateX(0)',
+          },
+        },
+        animation: {
+          sweep: 'sweep 1s infinite ease-in-out',
+        },
+      },
+
+      colors: {
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          1: 'hsl(var(--chart-1))',
+          2: 'hsl(var(--chart-2))',
+          3: 'hsl(var(--chart-3))',
+          4: 'hsl(var(--chart-4))',
+          5: 'hsl(var(--chart-5))',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+    },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require('tailwindcss-animate')],
 };
